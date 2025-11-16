d82ca05f14269979e21d825a46eca7e9e47a1334
nandini <2300033853@kluniversity.in>
2025-11-16 22:32:23 +0530

    update project

--- BACKEND/src/main/java/com/klef/devops/controller/VoterController.java
+++ BACKEND/src/main/java/com/klef/devops/controller/VoterController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.klef.devops.model.Voter;
@@ -18,6 +19,7 @@ import com.klef.devops.repository.VoterRepo;
 import com.klef.devops.service.VoterServiceImpl;
 
 @RestController
+@RequestMapping("/api")
 @CrossOrigin(origins ="*")
 public class VoterController {
 	
